887bce56afb8a1fecdc1b72a527b95be32f9e646
Toon Schoenmakers <nighteyes1993@gmail.com>
2019-12-02 21:16:04 +0100

    Extend to algorithm to account for fuel cost for fuel

--- src/day01/main.rs
+++ src/day01/main.rs
@@ -4,10 +4,21 @@ use std::error;
 use std::result::Result;
 use std::env;
 
-fn process_number(num: i64) -> i64 {
+fn process_fuel(num: i64) -> i64 {
     let out = (num / 3) - 2;
     if out < 0 {
         return 0;
+    }
+    out
+}
+
+fn fuel_for_fuel(num: i64) -> i64 {
+    let mut out: i64 = 0;
+    let mut fuel: i64 = process_fuel(num);
+    while fuel > 0 {
+        let extra = fuel;
+        out += fuel;
+        fuel = process_fuel(extra);
     }
     out
 }
@@ -19,7 +30,7 @@ fn process_file(file: &str) -> Result<i64, Box<dyn error::Error>> {
     let mut out: i64 = 0;
     for line in reader.lines() {
         let num: i64 = line?.trim().parse()?;
-        out += process_number(num);
+        out += fuel_for_fuel(num);
     }
     Ok(out)
 }
@@ -39,7 +50,6 @@ fn main() {
 }
 
 /*
-
     For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
     For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
     For a mass of 1969, the fuel required is 654.
@@ -48,8 +58,12 @@ fn main() {
 
 #[test]
 fn example() {
-    assert_eq!(process_number(12), 2);
-    assert_eq!(process_number(14), 2);
-    assert_eq!(process_number(1969), 654);
-    assert_eq!(process_number(100756), 33583);
+    assert_eq!(process_fuel(12), 2);
+    assert_eq!(process_fuel(14), 2);
+    assert_eq!(process_fuel(1969), 654);
+    assert_eq!(process_fuel(100756), 33583);
+
+    assert_eq!(fuel_for_fuel(14), 2);
+    assert_eq!(fuel_for_fuel(1969), 966);
+    assert_eq!(fuel_for_fuel(100756), 50346);
 }
