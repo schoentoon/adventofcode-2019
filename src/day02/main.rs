fcd242959b28f245c5639f630d12d3aa8960af39
Toon Schoenmakers <nighteyes1993@gmail.com>
2019-12-02 22:24:09 +0100

    Refactored everything a bit and solved part 2 of day 2 as well

--- src/day02/main.rs
+++ src/day02/main.rs
@@ -4,30 +4,31 @@ use std::error;
 use std::result::Result;
 use std::env;
 
-fn process_line(line: &str) -> Result<Vec<i32>, Box<dyn error::Error>> {
+fn process_line(line: &str) -> Result<Vec<usize>, Box<dyn error::Error>> {
     let mut out = Vec::new();
     let parser: Vec<&str> = line.split(',').collect();
     for opcode in parser {
-        let i: i32 = opcode.parse()?;
+        let i: usize = opcode.parse()?;
         out.push(i);
     }
     Ok(out)
 }
 
-fn process_opcodes(mut opcodes: Vec<i32>) -> Vec<i32> {
+fn process_opcodes(opcodes: Vec<usize>) -> Vec<usize> {
+    let mut opcodes = opcodes.clone();
     let mut pos: usize = 0;
     loop {
         match opcodes[pos] {
             1 => {
-                let modpos = opcodes[pos+3] as usize;
-                let pos1 = opcodes[pos+1] as usize;
-                let pos2 = opcodes[pos+2] as usize;
+                let modpos = opcodes[pos+3];
+                let pos1 = opcodes[pos+1];
+                let pos2 = opcodes[pos+2];
                 opcodes[modpos] = opcodes[pos1] + opcodes[pos2];
             },
             2 => {
-                let modpos = opcodes[pos+3] as usize;
-                let pos1 = opcodes[pos+1] as usize;
-                let pos2 = opcodes[pos+2] as usize;
+                let modpos = opcodes[pos+3];
+                let pos1 = opcodes[pos+1];
+                let pos2 = opcodes[pos+2];
                 opcodes[modpos] = opcodes[pos1] * opcodes[pos2];
             },
             99 => {
@@ -41,25 +42,41 @@ fn process_opcodes(mut opcodes: Vec<i32>) -> Vec<i32> {
     }
 }
 
-fn process_file(file: &str) -> Result<i32, Box<dyn error::Error>> {
+fn bruteforce_part2(opcodes: Vec<usize>) -> usize {
+    for noun in 1..99 {
+        for verb in 1..99 {
+            let mut opcodes = opcodes.clone();
+            opcodes[1] = noun;
+            opcodes[2] = verb;
+            if process_opcodes(opcodes)[0] == 19690720 {
+                return 100 * noun + verb;
+            }
+        }
+    }
+    return 0;
+}
+
+fn process_file(file: &str) -> Result<Vec<usize>, Box<dyn error::Error>> {
     let file = File::open(file)?;
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        let opcodes = process_line(&line?)?;
-        let out = process_opcodes(opcodes.clone());
-        return Ok(out[0]);
+        return process_line(&line?);
     }
-    Ok(0)
+    panic!("no intcodes?");
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let result = process_file(&args[1]);
+    let opcodes = process_file(&args[1]);
 
-    match result {
-        Ok(out) => {
-            println!("Output is {}", out);
+    match opcodes {
+        Ok(intcodes) => {
+            let out = process_opcodes(intcodes.clone());
+            println!("Output is {}", out[0]);
+
+            let result2 = bruteforce_part2(intcodes);
+            println!("Output of part 2 is {}", result2);
         },
         Err(error) => {
             println!("We encountered an error {}", error);
